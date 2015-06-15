16a44ad305067383972349a1d37bc005b0eda787
cmi <cmi@145-24-251-82.wlan.hro.nl>
2015-06-15 13:40:03 +0200

    added start region to map

---
+++ Finder/MainMap.swift
@@ -0,0 +1,13 @@
+//
+//  MainMap.swift
+//  Finder
+//
+//  Created by cmi on 15-06-15.
+//  Copyright (c) 2015 0851423. All rights reserved.
+//
+
+import UIKit
+
+class MainMap: MKMapView {
+   
+}
