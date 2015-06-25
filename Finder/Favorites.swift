1f82817e2f7053f0f6e172067426438bf16c8056
Hoppinger <hoppinger@Jims-iMac-2.local>
2015-06-25 20:37:39 +0200

    Added start of db

---
+++ Finder/Favorites.swift
@@ -0,0 +1,16 @@
+//
+//  Favorites.swift
+//  Finder
+//
+//  Created by Hoppinger on 6/25/15.
+//  Copyright (c) 2015 0851423. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Favorites: NSManagedObject {
+
+    @NSManaged var name: String
+
+}
--- Finder/ViewController.swift
+++ Finder/ViewController.swift
@@ -149,23 +149,37 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     
     func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
-        if let annotation = annotation as? User {
-            let identifier = "pin"
-            var view: MKPinAnnotationView
-            if let dequeuedView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
-                as? MKPinAnnotationView {
-                    dequeuedView.annotation = annotation
-                    view = dequeuedView
-            } else {
-                // 3
-                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-                view.canShowCallout = true
-                view.calloutOffset = CGPoint(x: -5, y: 5)
-                view.rightCalloutAccessoryView = UIButton.buttonWithType(.DetailDisclosure) as UIView
-                view.pinColor = .Green
-            }
-            return view
+        
+        if annotation is User {
+            return nil
         }
+        
+        if annotation is Garage {
+            let pinAnnotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pin")
+            
+            pinAnnotationView.pinColor = .Green
+            pinAnnotationView.draggable = true
+            pinAnnotationView.canShowCallout = true
+            pinAnnotationView.animatesDrop = true
+            
+            let deleteButton = UIButton.buttonWithType(UIButtonType.Custom) as UIButton
+            deleteButton.frame.size.width = 44
+            deleteButton.frame.size.height = 44
+            deleteButton.backgroundColor = UIColor.greenColor()
+            deleteButton.setImage(UIImage(named: "star"), forState: .Normal)
+            
+            pinAnnotationView.leftCalloutAccessoryView = deleteButton
+            
+            return pinAnnotationView
+        }
+        
+        
+        func mapView(mapView: MKMapView!, annotationView view: MKAnnotationView!, calloutAccessoryControlTapped control: UIControl!) {
+            if let annotation = view.annotation as? Garage {
+                self.mapView.removeAnnotation(annotation)
+            }
+        }
+        
         return nil
     }
 }
